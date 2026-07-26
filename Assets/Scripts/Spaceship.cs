1b6c57000a813c356d2200e7ad0a036373aadd9f
AlgarDev <diogotinocoferreira@gmail.com>
2026-07-26 19:43:33 +0200

    last shit

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -1,11 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.Animations;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Spaceship : MonoBehaviour
 {
